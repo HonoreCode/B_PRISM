3dc9181f49f99b5adf5a8ba8736b0cd012eaaf5f
honoré <honore.marmion@student-cs.fr>
2025-06-23 15:51:24 +0200

    add statistics and log output to test

--- model-checking basic test/testing_SICS.pl
+++ model-checking basic test/testing_SICS.pl
@@ -33,59 +33,98 @@ compare_floats_list([E1|L1],[E2|L2]) :-
 
 % Testing a formula with the "next" operator for the loop model
 test_loop_next :- 
-    sat(prob_formula(eq,0.9,x(prob_formula(eq,0.5,x(p(b))))),0).
+    (sat(prob_formula(eq,0.9,x(prob_formula(eq,0.5,x(p(b))))),0) -> 
+        print('Next formula suceeds\n')
+        ; print('Next formula fails\n')
+    ).
 
 % Testing a formula with the "until" operator for the loop model
 test_loop_until :- 
     findall(P,sat(prob_formula(eq,P,u(not(p(a)),p(b))),_S),List_P),
-    compare_floats_list(
+    (compare_floats_list(
         List_P,
         [0.783333333333334,0.0,0.870370370370371,0.0,1.0,0.944444444444445]
+        ) -> print('Until formula suceeds\n')
+            ; print('Until formula fails\n')
     ).
 
 % Testing a formula with the "until-bounded" operator for the loop model
 test_loop_until_bounded :-
-    findall(
+    (findall(
         P,
         sat(prob_formula(eq,P,u(not(p(a)),5,p(b))),_S),
         [0.73566,0.0,0.84304,0.0,1.0,0.90652]
+        ) -> print('Until-bounded formula suceeds\n')
+            ; print('Until-bounded formula fails\n')
     ).
 
 % Testing a formula with the "always" operator for the loop model
 test_loop_always :-
-    findall(S,sat(prob_formula(sup,0.1,g(not(p(b)))),S),[0,1,2,3]).
+    (findall(
+        S,
+        sat(prob_formula(sup,0.1,g(not(p(b)))),S),
+        [0,1,2,3]
+        ) -> print('Always formula suceeds\n')
+            ; print('Always formula fails\n')
+    ).
 
 % Testing a formula with the "eventually" operator for the loop model
 test_loop_eventually :- 
     findall(P,sat(prob_formula(eq,P,f(5,p(b))),_S),List_P),
-    compare_floats_list(
+    (compare_floats_list(
         List_P,
         [0.75978,0.28368,0.84304,0.0,1.0,0.90652]
+        ) -> print('Eventually formula suceeds\n')
+            ; print('Eventually formula fails\n')
     ).
 
 test_loop :- 
     assert(choose_model(loop)),
+    print('Begin loop model test\n'),
+    statistics(runtime,[T0|_]),
     test_loop_next,
     test_loop_until,
     test_loop_until_bounded,
     test_loop_always,
     test_loop_eventually,
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish loop model test. It took ~3d sec.~n \n',[T]),
     retract(choose_model(loop)).
 
 %**********************************************
 
 % The leader will be elected in one round :
 test_big_until_bounded :-
-    sat(prob_formula(eq,0.95703125,u(true,5,p(elect))),0).
+    (sat(
+        prob_formula(
+            eq,
+            0.95703125,
+            u(true,5,p(elect))),
+        0) -> print('Until-bounded formula suceeds\n')
+            ; print('Until-bounded formula fails\n')
+    ).
 
 % The leader will eventually be elected :
 test_big_until :-
-    sat(prob_formula(eq,1.0,u(true,p(elect))),0).
+    (sat(
+        prob_formula(
+            eq,
+            1.0,
+            u(true,p(elect))),
+        0) -> print('Until formula suceeds\n')
+            ; print('Until formula fails\n')
+    ).
 
 test_big :- 
     assert(choose_model(big)),
+    print('Begin leader election model test\n'),
+    statistics(runtime,[T0|_]),
     test_big_until,
     test_big_until_bounded,
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish leader election model test. It took ~3d sec.~n \n',[T]),
     retract(choose_model(big)).
 
 %***********************************************
