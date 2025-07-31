3fae68044d24213e2aaaa17df73a4d0886c120c8
honoré <honore.marmion@student-cs.fr>
2025-07-31 11:27:48 +0200

    testing of different version of the calculus of prob0

---
+++ model_checker_Prolog/archives/prob0_computation_test/dynamic_model_checking_prob0.pl
@@ -0,0 +1,304 @@
+%###############################################
+
+% This is a model-checker verifying Probabilistic Computation
+% Tree Logic (PCTL) formulas over Discrete-Time Markov Chains (DTMC)
+% This model-checker is the one used by ProB
+
+% The algorithm was greatly based on the PRISM's one.
+% See http://www.prismmodelchecker.org/lectures/pmc/
+
+% Working on SICStus prolog 4.10
+
+%###############################################
+
+:- module(dynamic_model_checking_prob0,[sat/2,sat/1,state/1]).
+:- use_module(library(clpr),[{}/1]).
+
+:- use_module(translate_model_prob0,[start/1,prop/2,state/1,trans/3]).
+
+%PCTL Model-checking
+
+
+% This term allow to handle nested formulas in case of dynamic model-checking
+:- dynamic node/1.
+
+
+sat(Formula):-start(E),sat(Formula,E).
+
+% Classic cases
+
+sat_not(false,_E).
+sat_not(true,_E) :- fail.
+sat_not(p(Property),E) :- \+(prop(Property,E)).
+sat_not(and(F,_G),E) :- sat_not(F,E).
+sat_not(and(_F,G),E) :- sat_not(G,E).
+sat_not(or(F,G),E) :- sat_not(F,E),sat_not(G,E).
+sat_not(implies(F,G),E) :- sat(not(or(not(F),G)),E).
+sat_not(not(F),E) :- sat(F,E).
+
+sat(true,_E).
+sat(false,_E):-fail.
+sat(p(Property),E) :- prop(Property,E).
+sat(and(F,G),E) :- sat(F,E), sat(G,E).
+sat(or(F,_G),E) :- sat(F,E).
+sat(or(_F,G),E) :- sat(G,E).
+sat(implies(F,G),E) :- sat(or(not(F),G),E).
+sat(not(F),E) :- 
+    F = probformula(Operator,P,Ctl_formula) ->
+        negate_operator(Operator,Neg_Operator),
+        sat(probformula(Neg_Operator,P,Ctl_formula))
+    ; sat_not(F,E).
+
+% Probabilistic-formula cases. Operator is =, < >,<= or >=. 
+%P is a number between 0 and 1 (or a Variable),
+% E is a state.
+
+% Check if the formula is nested, and handle the 
+% case of Always (g) and Always-bounded (gk) operators
+sat(probformula(Operator,P,Ctl_formula),E) :-
+    Ctl_formula = g1(_) ->
+        sat_g1(probformula(Operator,P,Ctl_formula),E)
+    ; Ctl_formula = g2(_) ->
+        sat_g2(probformula(Operator,P,Ctl_formula),E)
+    ; (node(Node) -> 
+        New_Node is Node +1,
+        retract(node(Node)),
+        assert(node(New_Node)),
+        sat_node(probformula(Operator,P,Ctl_formula),E,New_Node)
+    ;   assert(node(0)),
+        sat_node(probformula(Operator,P,Ctl_formula),E,0),
+        retractall(node(_))        /*reinitialize nodes*/
+    ).
+
+% Always formula, we use the opposite probabilistic event of fk(K,not(F))
+% and the associed opposite operator (different than the logically diferent one,
+% which is here called 'negation')
+sat_g1(probformula(Operator,P,g1(F)),E) :-
+    ground(P) -> 
+        opposed_operator(Operator,Opp_Operator),
+        Q is 1-P,
+        sat(probformula(Opp_Operator,Q,f1(not(F))),E)
+        
+    ;   opposed_operator(Operator,Opp_Operator),
+        sat(probformula(Opp_Operator,Q,f1(not(F))),E),
+        P is 1-Q
+    .
+
+sat_g2(probformula(Operator,P,g2(F)),E) :-
+    ground(P) -> 
+        opposed_operator(Operator,Opp_Operator),
+        Q is 1-P,
+        sat(probformula(Opp_Operator,Q,f2(not(F))),E)
+        
+    ;   opposed_operator(Operator,Opp_Operator),
+        sat(probformula(Opp_Operator,Q,f2(not(F))),E),
+        P is 1-Q
+    .
+
+% Check the type of the formula
+sat_node(probformula(Operator,P,Ctl_formula),E,Node) :- 
+    Ctl_formula = u1(F,G) ->
+        prob_calc_1(u1(F,G),E,P_phi,Node),
+        against(P_phi,P,Operator)
+    ;   Ctl_formula = f1(G) -> 
+        prob_calc_1(u1(true,G),E,P_phi,Node),
+        against(P_phi,P,Operator)
+    ; Ctl_formula = u2(F,G) ->
+        prob_calc_2(u2(F,G),E,P_phi,Node),
+        against(P_phi,P,Operator)
+    ;   Ctl_formula = f2(G) -> 
+        prob_calc_2(u2(true,G),E,P_phi,Node),
+        against(P_phi,P,Operator)
+    .
+
+% Compare different probabilities using a specific operator
+% For the equal comparison, we compare the results using epsilon precision in case
+% of a given probability
+against(P_phi,P,equal) :- 
+    ground(P) ->
+        P_phi =< P + 0.00000000000000023,
+        P =< P_phi + 0.00000000000000023
+    ;   P = P_phi
+    .
+against(P_phi,P,less) :- 
+    ground(P),
+    P_phi =< P + 0.00000000000000023.   % less
+against(P_phi,P,greater) :- 
+    ground(P),
+    P_phi >= P - 0.00000000000000023.   % greater
+against(P_phi,P,strictlyless) :- 
+    ground(P),
+    P_phi + 0.00000000000000023 < P.   % strictly less
+against(P_phi,P,strictlygreater) :- 
+    ground(P),
+    P_phi - 0.00000000000000023 > P .   % strictly greater
+
+% Used to compute the logical negation of
+% a probabilistic formula
+% General use : negate_operator(Op,not(Op))
+negate_operator(less,strictlygreater).
+negate_operator(strictlygreater,less).
+negate_operator(greater,strictlyless).
+negate_operator(strictlyless,greater).
+
+% As in PRISM, the negation of an equal comparison
+% will raise an error. Note that, however,
+% double negation is allowed
+negate_operator(equal,different). 
+
+
+% Used for formulas with 'Always' (G) Operator
+opposed_operator(equal,equal).
+opposed_operator(less,greater).
+opposed_operator(greater,less).
+opposed_operator(strictlygreater,strictlyless).
+opposed_operator(strictlyless,strictlygreater).
+
+
+% Until formula 
+% For this formula we have to calculate the probability for
+% all states
+prob_calc_1(u1(F,G),E,P_phi,Node) :- 
+    retractall(table_prob0_version1(_,_,Node)),
+    retractall(table_prob1_version1(_,_,Node)),
+    prob_calc_u_version_1(F,G,List_S,P_vect,Node),
+    (find_prob_u(List_S,P_vect,E,P) -> P_phi=P
+        ; P_phi=0.0
+    ).
+   
+prob_calc_2(u2(F,G),E,P_phi,Node) :- 
+    retractall(table_prob0_version2(_,Node)),
+    retractall(table_prob1_version2(_,_,Node)),
+    prob_calc_u_version_2(F,G,List_S,P_vect,Node),
+    (find_prob_u(List_S,P_vect,E,P) -> P_phi=P
+        ; P_phi=0.0
+    ).
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+% ~ ~ ~ ~ 1rst precomputation for the until formula ~ ~ ~ ~
+
+%      prob0 calculus VERSION 1
+
+% This is a graph-based computation to find 
+% all the states that can reach a state 
+% verifying a property G without leaving
+% states verifying a property F
+
+:- dynamic table_prob0_version1/3.
+
+prob0_version1(_F,_G,E,Node) :-
+    table_prob0_version1(E,true,Node),!.
+
+prob0_version1(_F,G,E,Node) :- 
+    sat(G,E),
+    !,
+    asserta(table_prob0_version1(E,true,Node)).
+
+prob0_version1(F,G,E1,Node) :- 
+    sat(F,E1),
+    assertz(table_prob0_version1(E1,computing,Node)),
+    trans(E1,E2,_P),
+    \+ table_prob0_version1(E2,computing,Node),
+    (prob0_version1(F,G,E2,Node) -> 
+        asserta(table_prob0_version1(E1,true,Node)),
+        retract(table_prob0_version1(E1,computing,Node))
+    ),!.
+
+% This part could definitely be improved to 
+% reduce computation time
+search_prob0_version1(F,G,E,Node):-
+    retractall(table_prob0_version1(_,computing,Node)),
+    prob0_version1(F,G,E,Node).
+    
+
+% - - - - - - - - - - - - - - - - - - - 
+
+%      prob0 calculus VERSION 2
+
+:- dynamic table_prob0_version2/2.
+
+prob0_version2(_F,[],_Node).
+
+prob0_version2(F,[E|T],Node) :-
+    trans(S,E,_P),
+    \+(table_prob0_version2(S,Node)),
+    sat(F,S),
+    assert(table_prob0_version2(S,Node)),
+    prob0_version2(F,[E,S|T],Node),!.
+
+prob0_version2(F,[_E|T]):-prob0_version2(F,T).
+
+
+% ~ ~ ~ ~ 2nd precomputation for the until formula ~ ~ ~ ~
+
+% This is a graph-based computation to find 
+% all the states that can reach a state 
+% which IS NOT verifying a property G without 
+% leaving states verifying a property F
+
+
+:- dynamic table_prob1_version1/3.
+
+prob1_version1(_F,_G,E,Node) :-
+    table_prob1_version1(E,true,Node),!.
+
+prob1_version1(_F,_G,E,Node) :- 
+    \+(table_prob0_version1(E,true,Node)),
+    !,
+    asserta(table_prob1_version1(E,true,Node)).
+
+prob1_version1(F,G,E1,Node) :- 
+    sat(F,E1),
+    \+(sat(G,E1)),
+    assert(table_prob1_version1(E1,computing,Node)),
+    trans(E1,E2,_P),
+    \+ table_prob1_version1(E2,computing,Node),
+    (prob1_version1(F,G,E2,Node) ->
+        asserta(table_prob1_version1(E1,true,Node)),
+        retract(table_prob1_version1(E1,computing,Node))
+    ),!.
+
+search_prob1_version1(F,G,E,Node):-
+    retractall(table_prob1_version1(_,computing,Node)),
+    prob1_version1(F,G,E,Node).
+
+
+%//////////////////////////////////////////////
+
+
+% Until formula computation
+
+% P_vect is the vector of non-null probabilities 
+% for the until formula
+prob_calc_u_version_1(F,G,List_S,P_vect,Node) :- 
+    findall(S,(state(S),search_prob0_version1(F,G,S,Node)),List_S),
+    findall(E,(state(E),search_prob1_version1(F,G,E,Node)),_List),
+    prob_calc_u2(F,G,List_S,List_S,P_vect,P_vect,Node).
+
+prob_calc_u_version_2(F,G,List_S,P_vect,Node) :- 
+    findall(S,(state(S),sat(G,S),assert(table_prob0_version2(S,Node))),T),
+    prob0_version2(F,T,Node),
+    findall(S,(table_prob0_version2(S,Node)),List_S),
+    findall(E,(state(E),search_prob1_version2(F,G,E,Node)),_List),
+    prob_calc_u2(F,G,List_S,List_S,P_vect,P_vect,Node).
+
+prob_calc_u2(_F,_G,[],_List_S,[],_P_vect,_Node).
+prob_calc_u2(F,G,[S|List_S_explored],List_S,[P_phi|P_vect_explored],P_vect,Node) :-
+    (table_prob1_version1(S,true,Node) -> prob_calc_u3(S,List_S,P_phi,P_vect) 
+        ;   P_phi=1.0
+    ),
+    prob_calc_u2(F,G,List_S_explored,List_S,P_vect_explored,P_vect,Node).
+
+prob_calc_u3(_S,[],0.0,[]).
+prob_calc_u3(S,[E|List_S],P_phi_new,[P|P_vect]) :-
+    prob_calc_u3(S,List_S,P_phi,P_vect),
+    (trans(S,E,P_trans) -> {P_phi_new = P*P_trans + P_phi}
+        ; P_phi_new=P_phi
+    ).
+ 
+find_prob_u([E],[P],E,P).
+find_prob_u([S1,S2|List_S],[Prob1,Prob2|P_vect],E,P) :- 
+    (E=S1,P=Prob1) 
+        ; find_prob_u([S2|List_S],[Prob2|P_vect],E,P).
---
+++ model_checker_Prolog/archives/prob0_computation_test/testing_prob0.pl
@@ -0,0 +1,293 @@
+%####################################################*
+
+% Test file to verify some formulas over the following models :
+
+%    - "loop" is a small model with a loop
+
+%    - "small" is an even smaller model
+
+%    - "big" is a big model from Prism's Benchmark suite :
+%          (Synchronous leader election protocol,
+%           parameters : N=4, K=8
+%           see http://www.prismmodelchecker.org/casestudies/synchronous_leader.php)
+
+%    - "crowds_5_3" is an bigger model from Prism's Benchmark suite :
+%          (Crowds Protocol,
+%           parameters : PF=0.8, badC=0.167, TotalRuns=3, CrowdSize=5
+%           see http://www.prismmodelchecker.org/casestudies/crowds.php)
+
+%    - "crowds_10_3" is an even bigger model from Prism's Benchmark suite :
+%          (Crowds Protocol,
+%           parameters : PF=0.8, badC=0.091, TotalRuns=3, CrowdSize=10
+%           see http://www.prismmodelchecker.org/casestudies/crowds.php)
+
+%   - If you want to test all the different models, run "?- full_test."
+
+
+%                WORKING WITH SICSTUS
+
+%####################################################
+
+:- module(testing_SICS,[choose_model/1,full_test/0,test_simple/0,test_loop/0,test_big/0,test_crowds_5_3/0,test_crowds_10_3/0]).
+
+:- use_module(dtmc_model_checking_SICS,[sat/2,sat/1,state/1]).
+
+:- use_module(library(clpr),[{}/1]).
+
+% Small predicate used to compare two floats lists
+% Needed because there are approximation error using float representation
+compare_floats_list([],[]).
+compare_floats_list([E1|L1],[E2|L2]) :-
+    {E2-0.000000000000005 =< E1},
+    {E1 =< E2+0.000000000000005},
+    compare_floats_list(L1,L2).
+
+
+% Small predicate used to compare two floats
+% Needed because there is a little difference 
+% between CLPFD equations solving and PRISM's one
+approx_float(F1,F2):-
+    {F1 =< 0.00001 + F2},
+    {F2 =< 0.00001 + F1}.
+
+
+:- dynamic choose_model/1.
+
+%******************************************
+
+% TESTS FOR THE LOOP MODEL
+
+
+% Testing a formula with the "until" operator for the loop model
+test_loop_until :- 
+    print('Begin loop until formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    findall(P,(state(S),sat(probformula(equal,P,u1(not(p(a)),p(b))),S)),List_P),
+    (compare_floats_list(
+        List_P,
+        [0.783333333333334,0.0,0.870370370370371,0.0,1.0,0.944444444444445]
+        ) -> print('Until formula version 1 SUCCEEDS\n')
+            ; print('Until formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish loop until formula checking version 1.\n It took ~3d sec.~n \n',[T]),
+    print('Begin loop until formula checking version 2\n'),
+    statistics(runtime,[T2|_]),
+    findall(P,(state(S),sat(probformula(equal,P,u2(not(p(a)),p(b))),S)),List_P),
+    (compare_floats_list(
+        List_P,
+        [0.783333333333334,0.0,0.870370370370371,0.0,1.0,0.944444444444445]
+        ) -> print('Until formula version 2 SUCCEEDS\n')
+            ; print('Until formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish loop until formula checking version 2.\n It took ~3d sec.~n \n',[T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+% Testing a formula with the "always" operator for the loop model
+test_loop_always :-
+    print('Begin loop always formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    (findall(
+        S,
+        (
+            state(S),
+            sat(probformula(greater,0.1,g1(not(p(b)))),S)
+        ),
+        [0,1,2,3]
+        ) -> print('Always formula version 1 SUCCEEDS\n')
+            ; print('Always formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish loop always formula checking version 1. It took ~3d sec.~n \n',[T]),
+    print('Begin loop always formula checking version 2 \n'),
+    statistics(runtime,[T2|_]),
+    (findall(
+        S,
+        (
+            state(S),
+            sat(probformula(greater,0.1,g2(not(p(b)))),S)
+        ),
+        [0,1,2,3]
+        ) -> print('Always formula version 2 SUCCEEDS\n')
+            ; print('Always formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish loop always formula version 2 checking. It took ~3d sec.~n \n',[T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+test_loop :- 
+    assert(choose_model(loop)),
+    print('Begin loop model tests\n\n'),
+    statistics(runtime,[T0|_]),
+    test_loop_until,!,
+    test_loop_always,!,
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish loop model tests. It took ~3d sec.~n \n\n',[T]),
+    retract(choose_model(loop)).
+
+% TESTS FOR THE SYNCHRONOUS LEADER ELECTION MODEL
+
+% The leader will eventually be elected :
+test_big_until :-
+    print('Begin leader election until formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    (sat(
+        probformula(
+            equal,
+            1.0,
+            u1(true,p(elect))),
+        0) -> print('Until formula version 1 SUCCEEDS\n')
+            ; print('Until formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish leader election until formula checking version 1. \n The result is ~h \n It took ~3d sec.~n \n',[1.0,T]),
+    print('Begin leader election until formula checking version 2\n'),
+    statistics(runtime,[T2|_]),
+    (sat(
+        probformula(
+            equal,
+            1.0,
+            u2(true,p(elect))),
+        0) -> print('Until formula version 2 SUCCEEDS\n')
+            ; print('Until formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish leader election until formula checking version 2. \n The result is ~h \n It took ~3d sec.~n \n',[1.0,T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+test_big :- 
+    assert(choose_model(big)),
+    print('Begin leader election model tests\n\n'),
+    statistics(runtime,[T0|_]),
+    test_big_until,!,
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish leader election model tests. It took ~3d sec.~n \n\n',[T]),
+    retract(choose_model(big)).
+
+
+%***********************************************
+
+% TESTS FOR THE CROWDS PROTOCOL MODEL WITH PAREMETERS 5 & 3
+
+% The adversary will eventually observe the real sender more than once
+
+test_crowds_5_3_positive :-
+    print('Begin crowds_5_3 positive formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    sat(probformula(equal,P,f1(p(positive))),0),
+    (approx_float(P,0.13834) -> print('positive formula version 1 SUCCEEDS\n')
+        ; print('positive formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish crowds_5_3 positive formula checking version 1. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    print('Begin crowds_5_3 positive formula checking version 2\n'),
+    statistics(runtime,[T2|_]),
+    sat(probformula(equal,P,f2(p(positive))),0),
+    (approx_float(P,0.13834) -> print('positive formula version 2 SUCCEEDS\n')
+        ; print('positive formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish crowds_5_3 positive formula checking version 2. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+test_crowds_5_3_false_positive :-
+    print('Begin crowds_5_3 false_positive formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    sat(probformula(equal,P,f1(p(false_positive))),0),
+    (approx_float(P,0.05104) -> print('false_positive formula version 1 SUCCEEDS\n')
+        ; print('false_positive formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish crowds_5_3 false_positive formula checking version 1. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    print('Begin crowds_5_3 false_positive formula checking version 2\n'),
+    statistics(runtime,[T2|_]),
+    sat(probformula(equal,P,f2(p(false_positive))),0),
+    (approx_float(P,0.05104) -> print('false_positive formula version 2 SUCCEEDS\n')
+        ; print('false_positive formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish crowds_5_3 false_positive formula checking version 2. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+test_crowds_5_3_confident :-
+    print('Begin crowds_5_3 confident formula checking version 1\n'),
+    statistics(runtime,[T0|_]),
+    sat(probformula(equal,P,f1(p(confident))),0),
+    (approx_float(P,0.13834) -> print('confident formula version 1 SUCCEEDS\n')
+        ; print('confident formula version 1 FAILS\n')
+    ),
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish crowds_5_3 confident formula checking version 1. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    print('Begin crowds_5_3 confident formula checking version 2\n'),
+    statistics(runtime,[T2|_]),
+    sat(probformula(equal,P,f2(p(confident))),0),
+    (approx_float(P,0.13834) -> print('confident formula version 2 SUCCEEDS\n')
+        ; print('confident formula version 2 FAILS\n')
+    ),
+    statistics(runtime,[T3|_]),
+    T_prime is T3-T2,
+    format('Finish crowds_5_3 confident formula checking version 2. \n The result is ~h \n It took ~3d sec.~n \n',[P,T]),
+    (T<T_prime ->
+        T_comp is T_prime-T,
+        format('For this formula, the version 1 model-checking is ~3d sec.~n faster than the version 2 model-checking \n\n\n',[T_comp])
+    ;   T_comp is T-T_prime,
+        format('For this formula, the version 2 model-checking is ~3d sec.~n faster than the version 1 model-checking \n\n\n',[T_comp])
+        )
+    .
+
+test_crowds_5_3 :-
+    assert(choose_model(crowds_5_3)),
+    print('Begin crowds_5_3 protocol model tests\n\n'),
+    statistics(runtime,[T0|_]),
+    test_crowds_5_3_positive,!,
+    test_crowds_5_3_false_positive,!,
+    test_crowds_5_3_confident,!,
+    statistics(runtime,[T1|_]),
+    T is T1-T0,
+    format('Finish crowds_5_3 protocol tests. It took ~3d sec.~n \n\n',[T]),
+    retract(choose_model(crowds_5_3)).
+
